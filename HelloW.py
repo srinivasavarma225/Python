c9c42499b29c58cb0a040e5a99e194e0298c8a70
srinivasavarma225 <153558370+srinivasavarma225@users.noreply.github.com>
2025-06-03 17:45:17 -0500

    commit

---
+++ HelloW.py
@@ -0,0 +1,13 @@
+name  = 'hello world'
+
+print(s[1]) # e
+print(s[-1]) # d
+print(s[1:3]) # el
+print(s[1:-1]) # ello worl
+print(s[:3]) # hel
+print(s[2:]) # llo world
+print(s[:-1]) # hello worl
+print(s[::2]) # hlo ol
+print(s[1::2]) # el ol
+print(s[::-1]) 
+
